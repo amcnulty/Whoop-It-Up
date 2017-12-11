b75fe6747d41d3dd719136d0197cc00969eb4be8
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-12-10 16:20:37 -0800

    fixed typo on landing page

---
+++ public/javascripts/signup.js
@@ -0,0 +1,19 @@
+var WIU = WIU || {};
+
+WIU.signup = (function() {
+
+  var 
+  init = function() {
+    if ($('.signup-page').length) {
+      
+    }
+  };
+
+  return {
+    init : init
+  }
+})();
+
+$(function() {
+  WIU.signup.init();
+});
