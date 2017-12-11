b9714819c04993a3d9532257bd169ad534238ec0
Brendan Kellams <brendan.kellams@gmail.com>
2017-12-10 16:08:38 -0800

    worked on events.scss, pageRoutes.js and events.hbs

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -3,20 +3,36 @@ var router = express.Router();
 var db = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', {});
 });
 
-router.get('/create-event', function(req, res, next) {
+router.get('/create-event', function (req, res, next) {
   res.render('index', { title: 'Create Event' });
 });
 
-router.get('/find-events', function(req, res, next) {
+router.get('/find-events', function (req, res, next) {
   res.render('find-events', { title: 'Find Events' });
 });
 
-router.get('/signup', function(req, res, next) {
+router.get('/signup', function (req, res, next) {
   res.render('signup', { title: 'Signup' });
 });
 
-module.exports = router;
+router.get('/events', function (req, res, next) {
+  res.render('events', {
+    title: 'Events',
+    events: [{
+      name: "Andy's Lan Party",
+      date: "12/21/2017",
+      location: "Andy's Place",
+      description: "This is the best LAN party in the world!"
+    }, {
+      name: "Brendan's Pool Party",
+      date: "12/21/2017",
+      location: "Brendan's Place",
+      description: "This is the best POOL party in the world!"
+    }]
+  });
+});
+  module.exports = router;
