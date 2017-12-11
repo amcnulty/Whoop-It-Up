31a41001e2e39addba84fed5de8666e8bb49841d
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-12-10 18:04:21 -0800

    basic setup for create event, added verification

--- public/javascripts/create-event.js
+++ public/javascripts/create-event.js
@@ -3,9 +3,55 @@ var WIU = WIU || {};
 WIU.createEvent = (function() {
 
   var 
+  geocodeAddress = function() {
+    //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+  },
+  initDatepicker = function() {
+    $('#event-date').datepicker();
+  },
+  verifyData = function(obj) {
+    return !(typeof(obj.name) === 'undefined' || obj.name === null || obj.name.trim() === '');
+  },
+  getData = function() {
+    var page = '.create-events-page',
+        eventName = $('.event-name', page).val(),
+        eventDate = $('.event-date', page).val(),
+        eventTime = $('.start-time', page).val(),
+        location  = $('.location', page).val(),
+        eventDesc = $('.event-desc', page).val(),
+        invites   = $('.invite', page).val();
+
+    return {
+      name      : eventName,
+      date      : eventDate,
+      time      : eventTime,
+      location  : location,
+      desc      : eventDesc,
+      invites   : invites
+    };
+  },
+  initCreateBtn = function() {
+    var $createBtn = $('.create-btn');
+
+    $createBtn.on('click', function(e) {
+      var eventObj = getData();
+
+      if (verifyData(eventObj)) {
+      }
+      else {
+        // throw warning!
+        $('.error-name').removeClass('hidden');
+
+        return false;
+      }
+
+    });
+  },
   init = function() {
-    if ($('.signup-page').length) {
-      
+    if ($('.create-events-page').length) {
+      console.log('!!!');  
+      initDatepicker();
+      initCreateBtn();
     }
   };
 
