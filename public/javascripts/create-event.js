01c6273c9e66206dd1a816fa4c3c09c31ec28367
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-12-10 21:01:08 -0800

    basic create event page completed. Added basic geocode

--- public/javascripts/create-event.js
+++ public/javascripts/create-event.js
@@ -3,8 +3,80 @@ var WIU = WIU || {};
 WIU.createEvent = (function() {
 
   var 
-  geocodeAddress = function() {
-    //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+  isSimilar = function(original, formatted) {
+    var ogArr = original.split(' '),
+        total = ogArr.length,
+        matched = 0;
+
+    $.each(ogArr, function(index, word) {
+      if (formatted.indexOf(word) !== -1) {
+        matched++;
+      }
+    });
+    console.log('matched ' + matched + ' out of ' + total);
+
+    return (matched/total).toFixed(2);
+  },
+  geocodeAddress = function(location, callback) {
+    var apiKey = 'AIzaSyCdPObqQECeLB2K0xW96U8Rhzd3sFS9d4k',
+        address = location.split(' ').join('+'),
+        apiURL = 'https://maps.googleapis.com/maps/api/geocode/json?components=country:US&',
+        requestURL = apiURL + 'address=' + address + '&key=' + apiKey,
+        addressObj = {};
+
+    console.log('restURL: ', requestURL);
+
+    $.ajax({
+      method: "GET",
+      url: requestURL
+    })
+    .done(function(data) {
+      console.log('what google said:', data);
+      if (data.status === 'OK' && data.results.length == 1) {
+        var lat = data.results[0].geometry.location.lat,
+            lng = data.results[0].geometry.location.lng,
+            placeID = data.results[0].place_id,
+            formattedAddy = data.results[0].formatted_address;
+
+        //console.log('hey', lat, lng, placeID, formattedAddy);
+
+        addressObj.latlng     = lat +', ' + lng;
+        addressObj.placeID    = placeID;
+        addressObj.formatted  = formattedAddy;
+
+        if (typeof callback == 'function') {
+          callback(addressObj);
+        }
+      }
+      else {
+        console.log('problem geocoding', data);
+      }
+
+    });
+  },
+  showSuggestion = function(addressObj) {
+    var $suggestDiv = $('.address-suggest-div'),
+        $addyDiv = $('.address-suggest', '.address-suggest-div');
+
+    if (typeof addressObj.formatted !== 'undefined' && addressObj.formatted != '') {
+      $addyDiv.html(addressObj.formatted);
+      $suggestDiv.removeClass('hidden');
+    }
+  },
+  initAddressComplete = function() {
+    var $location = $('.location', '.create-events-page');
+
+    $location.on('blur', function() {
+
+      var address = $location.val();
+
+      if (typeof address !== 'undefined' && address.trim() != '' && address.trim().length > 1) {
+        geocodeAddress(address.trim(), function(obj) {
+          showSuggestion(obj);
+        });
+      }
+      
+    })
   },
   initDatepicker = function() {
     $('#event-date').datepicker();
@@ -19,7 +91,8 @@ WIU.createEvent = (function() {
         eventTime = $('.start-time', page).val(),
         location  = $('.location', page).val(),
         eventDesc = $('.event-desc', page).val(),
-        invites   = $('.invite', page).val();
+        invites   = $('.invite', page).val(),
+        isPrivate = $('.is-private-cb', page).is(':checked');
 
     return {
       name      : eventName,
@@ -27,6 +100,7 @@ WIU.createEvent = (function() {
       time      : eventTime,
       location  : location,
       desc      : eventDesc,
+      private   : isPrivate,
       invites   : invites
     };
   },
@@ -37,6 +111,7 @@ WIU.createEvent = (function() {
       var eventObj = getData();
 
       if (verifyData(eventObj)) {
+        //geocodeAddress(eventObj.location);
       }
       else {
         // throw warning!
@@ -49,9 +124,9 @@ WIU.createEvent = (function() {
   },
   init = function() {
     if ($('.create-events-page').length) {
-      console.log('!!!');  
       initDatepicker();
       initCreateBtn();
+      initAddressComplete();
     }
   };
 
