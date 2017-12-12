9650bfddc449410609aabeceb996847aae48dd6b
Brendan Kellams <brendan.kellams@gmail.com>
2017-12-11 22:55:57 -0800

    working on signup.js

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -22,10 +22,8 @@ $(function() {
 // email validation
 function validateEmail(email) {
   var emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
   return emailFormat.test(email);
 }
-
 function validate() {
   $('#result').text('');
   var email = $('#email').val();
@@ -39,4 +37,17 @@ function validate() {
   return false;
 }
 
+// confirming both passwords entered are the same
+function confirmPW() {
+  var pw = $('#password').val();
+  var pwConfirm = $('#confirm-pw').val();
+
+  if (pw === pwConfirm) {
+    pw.push(/*user information database/array */);
+  } else {
+    alert("Passwords don't match.  Please try again.");
+  }
+}
+
 $('#signUpBtn').on('click', validate);
+$('#signUpBtn').on('click', confirmPW);
