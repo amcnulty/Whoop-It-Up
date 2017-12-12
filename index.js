b79a5273c1f8f1e5365c5a4b166bca89cb07992e
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-12-11 20:00:11 -0800

    basic edit profile page

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/profile', express.static('public'));
 
 app.use('/', pageRoutes);
 app.use('/profile', profile);
