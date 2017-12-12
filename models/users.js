3f889eff8304854fb303689a20fb86c380774b44
shubha shankar <shankar.shub@gmail.com>
2017-12-11 18:56:37 -0800

    Add users columns

--- models/users.js
+++ models/users.js
@@ -1,7 +1,46 @@
 module.exports = function(sequelize, DataTypes) {
     var Users = sequelize.define("Users", {
-        // columns go here
-    });
+    	username: {
+    	    type: DataTypes.STRING,
+      		allowNull: false,
+      		validate: {
+        	len: [1]
+	    	}
+    	},
+    	password: {
+    	    type: DataTypes.STRING,
+      		allowNull: false,
+      		validate: {
+        	len: [1]
+	    	}
+    	},
+    	fullname: {
+    	    type: DataTypes.STRING,
+      		allowNull: false,
+      		validate: {
+        	len: [1]
+	    	}
+    	},
+    	email: {
+    	    type: DataTypes.STRING,
+    	    primaryKey: true,
+      		allowNull: false,
+      		validate: {
+        	len: [1]
+	    	}
+    	},
+    	avatar: {
+    	    type: DataTypes.STRING,
+      		allowNull: true
+    	},
+    	signupdate: {
+    		type: DataTypes.DATE
+    	},
+    	lastchgdate: {
+    		type: DataTypes.DATE
+    	}
+
+     });
 
     Users.associate = function(model) {
         Users.belongsToMany(model.Events, {
