47f8260c0a28ca6d49d01bb04f52dc166f863ba6
Brendan Kellams <brendan.kellams@gmail.com>
2017-12-11 22:35:16 -0800

    worked on email validation

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -17,3 +17,26 @@ WIU.signup = (function() {
 $(function() {
   WIU.signup.init();
 });
+
+
+
+function validateEmail(email) {
+  var emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return emailFormat.test(email);
+}
+
+function validate() {
+  $('#result').text('');
+  var email = $('#email').val();
+  if (validateEmail(email)) {
+    $('#reult').text(email + " is valid");
+    $('#result').css('color', "green");
+  } else {
+    $('#result').text(email + " is not valid");
+    $('#result').css('color', 'red');
+  }
+  return false;
+}
+
+$('#signUpBtn').on('click', validate);
