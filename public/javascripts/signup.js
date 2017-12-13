d5de1d8448d3e08b63762eb196da1131802c5d5d
Brendan Kellams <brendan.kellams@gmail.com>
2017-12-12 17:18:01 -0800

    changes to signup.js

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -19,41 +19,44 @@ $(function () {
 });
 
 
-// email validation
-function validateEmail(email) {
-  var emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailFormat.test(email);
-}
-function validate() {
-  $('#result').text('');
-  var email = $('#email').val();
-  if (validateEmail(email)) {
-    $('#reult').text(email + " is valid");
-    $('#result').css('color', "green");
-  } else {
-    $('#result').text(email + " is not valid");
-    $('#result').css('color', 'red');
-  }
-  return false;
-}
 
-// confirming both passwords entered are the same
-// function confirmPW() {
-//   var pw = $('#password').val();
-//   var pwConfirm = $('#confirm-pw').val();
 
-//   if (pw === pwConfirm) {
-//     pw.push(/*user information database/array */);
-//   } else {
-//     alert("Passwords don't match.  Please try again.");
-//   }
-// }
 
-// $('#signUpBtn').on('click', validate);
-// $('#signUpBtn').on('click', confirmPW);
+
+
+$('#signUpBtn').on('click', validate);
+$('#signUpBtn').on('click', confirmPW);
 
 
 function addUser() {
+  // email validation
+  function validateEmail(email) {
+    var emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailFormat.test(email);
+  }
+  function validate() {
+    $('#result').text('');
+    var email = $('#email').val();
+    if (validateEmail(email)) {
+      $('#reult').text(email + " is valid");
+      $('#result').css('color', "green");
+    } else {
+      $('#result').text(email + " is not valid");
+      $('#result').css('color', 'red');
+    }
+    return false;
+  }
+  // confirming both passwords entered are the same
+  // function confirmPW() {
+  //   var pw = $('#password').val();
+  //   var pwConfirm = $('#confirm-pw').val();
+
+  //   if (pw === pwConfirm) {
+  //     console.log(pw);
+  //   } else {
+  //     alert("Passwords don't match.  Please try again.");
+  //   }
+  // }
   $('#signUpBtn').on('click', function () {
     var newUser = {};
     $('input').each(function () {
