f20a24f8d8690bec40ae0a2ff17e297ac33a6c67
Brendan Kellams <brendan.kellams@gmail.com>
2017-12-14 00:06:36 -0800

    made changes to handlebars and header.js

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,27 +1,62 @@
 var WIU = WIU || {};
 
-WIU.header = (function() {
+WIU.header = (function () {
 
-  var 
-  init = function() {
-    if ($('.site-header.nav').length) {
-      
+  var
+    init = function () {
+      if ($('.site-header.nav').length) {
+        $('#signUpBtn').on('click', function () {
+          if (!validate()) {
+            $('#result').text("Sorry! This email is not a valid email address");
+          }
+          else if (emptyPassword()) {
+            $('#result').text("Please enter in a password.");
+          }
+          else {
+            existingUser();
+          }
+        });
+      }
+    },
+    structureEmail = function (email) {
+      var emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return emailFormat.test(email);
+    },
+    validate = function () {
+      $('#result').text('');
+      var email = $('.email').val();
+      if (structureEmail(email)) {
+        return true;
+      } else {
+        $('#result').text(email + " is not valid");
+        $('#result').css('color', 'red');
+        return false;
+      }
+    },
+    emptyPassword = function () {
+      var pw = $('.password').val();
+      if (pw === "") {
+        return true;
+      }
+      else {
+        return false;
+      }
+    },
+    existingUser = function () {
+      var user = {
+        email: $('.email').val(),
+        password: $('.password').val()
+      };
+      console.log(user);
     }
-  };
 
   return {
-    init : init
+    init: init
   }
 })();
 
-$(function() {
+$(function () {
   WIU.header.init();
 });
 
-validateEmail = function() {
 
-}
-
-validatePW = function() {
-
-}
