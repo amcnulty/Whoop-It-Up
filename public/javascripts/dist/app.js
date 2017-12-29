bbfca9694e8d00072a6a0b46f5e53c2d6b5f4d71
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-12-29 01:31:32 -0500

    added animation to site logo

--- public/javascripts/dist/app.js
+++ public/javascripts/dist/app.js
@@ -1123,6 +1123,14 @@ WIU.site = (function () {
     startTitleAnimate = function() {
       WIU.animate.apply($('h2.fun-font'), 'tada'); 
     },
+    playSiteLogoAnimate = function() {
+      var effects = ['swing', 'bounce', 'flash', 'jello', 'rubberBand', 'pulse'],
+          $siteLogo = $('.site-logo');
+
+      if ($siteLogo.length) {
+        WIU.animate.apply($siteLogo, effects[getRand(0, effects.length-1)]);
+      }
+    },
     init = function () {
       putBackground($('.top-region'));
       putBackground($('.bottom-region'));
@@ -1130,6 +1138,8 @@ WIU.site = (function () {
       WIU.animate.bkgSlideIn($('.top-region'));
       WIU.animate.bkgSlideIn($('.bottom-region'));
 
+      var animteSiteLogo = setInterval(playSiteLogoAnimate, 4500);
+      
       startTitleAnimate();
     };
 
