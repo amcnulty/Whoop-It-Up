63ad9f62eb58a744686beb5c62afa746db505b5d
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2018-01-01 11:38:51 -0500

    modified frontend to work with edit-event

--- public/javascripts/event.js
+++ public/javascripts/event.js
@@ -10,18 +10,36 @@ WIU.event = (function () {
       
       $fbBtn.attr('href', shareURL + currentURL);
     },
+    verifyData = function(obj) {
+      return !(typeof(obj.name) === 'undefined' || obj.name === null || obj.name.trim() === '');
+    },
     bindUpdateButton = function() {
       $('.update-btn', '.button-row').on('click', function(e) {
-        $.ajax({
-          method: 'POST',
-          url: '../event/addinvite',
-          data: {
-            eventId: window.location.href.match(/\d*$/)[0],
-            username: $('#inviteUser').val()
-          }
-        }).done(function(res) {
-          WIU.animate.leavePage('/profile/getuser/' + $('#hostLabel').attr('data-id'));
-        });
+        var eventObj = getData();
+
+        if (verifyData(eventObj)) {
+
+          console.log('edit Event', eventObj);
+          // return false;
+
+          $.ajax({
+            method: 'POST',
+            url: '../event/addinvite',
+            data: {
+              eventId: window.location.href.match(/\d*$/)[0],
+              username: $('#inviteUser').val()
+            }
+          }).done(function(res) {
+            WIU.animate.leavePage('/profile/getuser/' + $('#hostLabel').attr('data-id'));
+          });
+        }
+        else {
+          // throw warning!
+          $('.error-name').removeClass('hidden');
+
+          return false;
+        }
+        
       });
     },
     bindDeleteButton = function() {
@@ -40,7 +58,6 @@ WIU.event = (function () {
     getPlaceID = function() {
       return $('.place-id').val();
     }, 
-    
     readyStaticMap = function() {
       var key = 'AIzaSyAxQrQpPeVTP0SFDubCMCNtDaRqOjM-fxk',
           latlong = getLatLong(),
@@ -71,16 +88,63 @@ WIU.event = (function () {
       else {
         // no map
       }
-
     },
-    eventCategoryData = function() {
-      $.ajax({
-        method: 'POST',
-        url: '',
-        data: page
-      }).done(function() {
-        window.location = window.location.href;
-      })
+    getEventCategory = function() {
+      var categories = $('.event-category'),
+          checkedCategories = [];
+
+      for (var i = 0; i < categories.length; i++) {
+        if ($(categories[i]).is(':checked')) checkedCategories.push(
+          parseInt(categories[i].value)
+        );
+      }
+
+      if (checkedCategories.length == 0) {
+        checkedCategories.push(7);
+      }
+
+      return checkedCategories;
+    },
+    getData = function() {
+      var page = '.event-page',
+          eventName = $('.event-name', page).val(),
+          eventDate = $('.event-date', page).val(),
+          eventTime = $('.start-time', page).val(),
+          location  = $('.location', page).val(),
+          eventDesc = $('.event-desc', page).val(),
+          isPrivate = $('.is-private-cb', page).is(':checked'),
+          checkedCategories = getEventCategory();
+
+          suggestCB = $('.suggest-cb', page).is(':checked'),
+          locationObj = {
+            name : location
+          };
+
+      if (suggestCB) {
+        locationObj.placeID = $('.place-id-new', page).val();
+        locationObj.latlng  = $('.latlong-new', page).val();
+        locationObj.formatted = $('.formatted-addy-new', page).val();
+      }
+      else {
+        locationObj.placeID = $('.place-id', page).val();
+        locationObj.latlng  = $('.latlong', page).val();
+        locationObj.formatted = $('.formatted-addy', page).val();
+      }
+
+      return {
+        name      : eventName,
+        date      : eventDate,
+        time      : eventTime,
+        location  : location,
+        description : eventDesc,
+        isPrivate   : isPrivate,
+        geoData   : locationObj,
+        categories: JSON.stringify(checkedCategories),
+        invite    : {
+          username  : $('#inviteUser').val(),
+          eventId   : window.location.href.match(/\d*$/)[0],  
+        }
+      };
     },
     populateAutocomplete = function() {
       var userNames = [];
@@ -99,6 +163,84 @@ WIU.event = (function () {
         });
       });
     },
+    geocodeAddress = function(location, callback) {
+      var apiKey = 'AIzaSyCdPObqQECeLB2K0xW96U8Rhzd3sFS9d4k',
+          address = location.split(' ').join('+'),
+          apiURL = 'https://maps.googleapis.com/maps/api/geocode/json?components=country:US&',
+          requestURL = apiURL + 'address=' + address + '&key=' + apiKey,
+          addressObj = {};
+
+      $.ajax({
+        method: "GET",
+        url: requestURL
+      })
+      .done(function(data) {
+        if (data.status === 'OK' && data.results.length == 1) {
+          var lat = data.results[0].geometry.location.lat,
+              lng = data.results[0].geometry.location.lng,
+              placeID = data.results[0].place_id,
+              formattedAddy = data.results[0].formatted_address;
+
+          addressObj.latlng     = lat +', ' + lng;
+          addressObj.placeID    = placeID;
+          addressObj.formatted  = formattedAddy;
+        }
+        else {
+          console.log('problem geocoding', data);
+        }
+
+        if (typeof callback == 'function') {
+          callback(addressObj);
+        }
+      });
+    },
+    showSuggestion = function(addressObj) {
+      var $suggestDiv = $('.address-suggest-div'),
+          $addyDiv = $('.address-suggest', '.address-suggest-div'),
+          $suggestCB = $('.suggest-cb', '.address-suggest-div');
+
+      // clear check box and hide suggestion
+      $suggestCB.prop('checked', false);
+      $suggestDiv.addClass('hidden');
+
+      if (typeof addressObj.formatted !== 'undefined' && addressObj.formatted != '') {
+        $addyDiv.html(addressObj.formatted);
+        $suggestDiv.removeClass('hidden');
+      }
+    },
+    fillHiddenFields = function(obj){ 
+      var page = '.event-page',
+          $placeID = $('.place-id-new', page),
+          $latlong = $('.latlong-new', page),
+          $formatted = $('.formatted-addy-new', page);
+
+      if (typeof obj.placeID !== 'undefined' && typeof obj.latlng !== 'undefined' &&
+          typeof obj.formatted !== 'undefined') {
+        $placeID.val(obj.placeID);
+        $latlong.val(obj.latlng);
+        $formatted.val(obj.formatted);  
+      }
+      else {
+        $placeID.val('');
+        $latlong.val('');
+        $formatted.val('');
+      }
+    },
+    initAddressComplete = function() {
+      var $location = $('.location', '.edit-event');
+
+      $location.on('blur', function() {
+
+        var address = $location.val();
+
+        if (typeof address !== 'undefined' && address.trim() != '' && address.trim().length > 1) {
+          geocodeAddress(address.trim(), function(obj) {
+            fillHiddenFields(obj);
+            showSuggestion(obj);
+          });
+        }
+      });
+    },
     init = function () {
       if ($('.event-page').length) {
         bindFacebookShare();
@@ -106,6 +248,7 @@ WIU.event = (function () {
         bindDeleteButton();
         populateAutocomplete();
         readyStaticMap();
+        initAddressComplete();
         //readyGoogleMap();
       }
     };
