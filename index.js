2e4a3ab5034a3f5bb9104ab3ce3b36f2f73936d9
amcnulty <amcnulty88@swbell.net>
2018-01-06 07:53:06 -0800

    Maintain Data

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use('/', pageRoutes);
 app.use('/profile', profile);
 app.use('/event', event);
 // Create database connection and start server on predefined port.
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
